4a4445106a2941665b74015e45b9f5849543626f
Henrique Artur <henriqueacg@gmail.com>
2020-08-22 23:00:26 -0300

    minimal test server

--- public/game.js
+++ public/game.js
@@ -8,41 +8,89 @@ export default function createGame() {
         },
     }
 
+    const observers = []
+
+    function start() {
+        const frequency = 2000
+
+        setInterval(addFruit, frequency)
+    }
+
+    function subscribe(observerFunction) {
+        observers.push(observerFunction)
+    }
+
+    function notifyAll(command) {
+        for (const observerFunction of observers) {
+            observerFunction(command)
+        }
+    }
+
+    function setState(newState) {
+        Object.assign(state, newState)
+    }
+
     function addPlayer(command) {
         const playerId = command.playerId
-        const playerX = command.playerX
-        const playerY = command.playerY
+        const playerX = 'playerX' in command ? command.playerX : Math.floor(Math.random() * state.screen.width)
+        const playerY = 'playerY' in command ? command.playerY : Math.floor(Math.random() * state.screen.height)
 
         state.players[playerId] = {
             x: playerX,
             y: playerY
         }
+
+        notifyAll({
+            type: 'add-player',
+            playerId: playerId,
+            playerX: playerX,
+            playerY: playerY
+        })
     }
 
     function removePlayer(commnad) {
         const playerId = commnad.playerId
 
         delete state.players[playerId]
+
+        notifyAll({
+            type: 'remove-player',
+            playerId: playerId
+        })
     }
     
     function addFruit(command) {
-        const fruitId = command.fruitId
-        const fruitX = command.fruitX
-        const fruitY = command.fruitY
+        const fruitId = command ? command.fruitId : Math.floor(Math.random() * 10000000)
+        const fruitX = command ? command.fruitX : Math.floor(Math.random() * (state.screen.width + 1))
+        const fruitY = command ? command.fruitY : Math.floor(Math.random() * (state.screen.height + 1))
 
         state.fruits[fruitId] = {
             x: fruitX,
             y: fruitY
         }
+
+        notifyAll({
+            type: 'add-fruit',
+            fruitId: fruitId,
+            fruitX: fruitX,
+            fruitY: fruitY
+        })
     }
 
     function removeFruit(commnad) {
         const fruitId = commnad.fruitId
 
         delete state.fruits[fruitId]
+
+        notifyAll({
+            type: 'remove-fruit',
+            fruitId: fruitId,
+        })
     }
     
     function movePlayer(command) {
+        notifyAll(command)
+        
         const acceptedMoves = {
             ArrowUp(player) {                            
                 player.y = Math.max(player.y - 1, 0)
@@ -66,7 +114,6 @@ export default function createGame() {
         if(player && moveFunction) {
             moveFunction(player)
             checkForFruitCollision(playerId)
-            console.log(`player: ${playerId}, x: ${player.x}, y: ${player.y}`)
         }
     }
 
@@ -85,9 +132,12 @@ export default function createGame() {
     return {
         addPlayer,
         removePlayer,
+        movePlayer,
         addFruit,
         removeFruit,
-        movePlayer,
-        state
+        state,
+        setState,
+        subscribe,
+        start,
     }
 }
--- server.js
+++ server.js
@@ -3,26 +3,40 @@ import http from 'http'
 import socketio from 'socket.io'
 
 import createGame from './public/game.js'
+import { Socket } from 'dgram'
 
 const app = express()
 const server = http.createServer(app)
 const sockets = socketio(server)
 
 app.use(express.static('public'))
-
 const game = createGame()
-game.addPlayer({playerId: 'player1', playerX: 0, playerY: 0})
-game.addFruit({fruitId: 'fruit1', fruitX: 8, fruitY: 8})
+game.start()
 
-game.movePlayer({playerId: 'player1', keyPressed: 'ArrowRight'})
-
-console.log(game.state)
+game.subscribe((command) => {
+    console.log(`> Emitting ${command.type}`)
+    sockets.emit(command.type, command)
+})
 
 sockets.on('connection', (socket) => {
     const playerId = socket.id
     console.log(`> Player connected on Server with id: ${playerId}`)
 
+    game.addPlayer({playerId: playerId})
+
     socket.emit('setup', game.state)
+
+    socket.on('disconnect', () => {
+        game.removePlayer({playerId: playerId})
+        console.log(`> Player disconnected: ${playerId}`)
+    })
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId
+        command.type = 'move-player'
+
+        game.movePlayer(command)
+    })
 })
 
 server.listen(3000, () => {
