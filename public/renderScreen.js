7c796569943f0c29f4dea82938d6116a7509441f
Henrique Artur <contato@henriqueartur.com>
2020-10-12 17:51:20 -0300

    render screen

--- public/renderScreen.js
+++ public/renderScreen.js
@@ -1,21 +1,20 @@
 export default function renderScreen(game, currentPlayerId) {
-    const currentPlayer = game.state.players[currentPlayerId]
     const oponentHandDiv = document.getElementById('oponent-hand')
-
-    const players = game.state.players
+    const players = Object.entries(game.state.players)
 
     players.forEach((player) => {
-        if(currentPlayer == player[currentPlayer]) {
+        if(currentPlayerId == player[0]) {
             const playerHandDiv = document.getElementById('my-hand')
             let playerCards = ''
+            const currentPlayer = game.state.players[player[0]]
             
             currentPlayer.hand.forEach( card => {
                 playerCards += `
                 <div class="my-card front">
                     <div class="symbol">
-                        <i class="material-icons small">${card.symbol}</i>
+                        <i class="material-icons small">${card['symbol']}</i>
                     </div>
-                    <p class="number">${card.value}</p>
+                    <p class="number">${card['value']}</p>
                 </div>
                 `
             })
@@ -23,7 +22,8 @@ export default function renderScreen(game, currentPlayerId) {
             playerHandDiv.innerHTML = playerCards
         } else {
             let oponentCards = ''
-            for(let i = player.hand.length; i >= 1; i--) {
+            const oponent = Object.entries(player[1])
+            for(let i = oponent[0][1].length; i >= 1; i--) {
                 oponentCards += `
                     <div class="my-card verse">
                         <div class="back"><i class="material-icons large">dashboard</i></div>
@@ -33,6 +33,5 @@ export default function renderScreen(game, currentPlayerId) {
             oponentHandDiv.innerHTML = oponentCards
         }
     })
-
 
 }
